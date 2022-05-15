66eec2da604cbf1630e712f69074488aac353bc1
Mohamed Nur <mohamed.nur@cds-snc.ca>
2022-05-15 18:55:32 -0400

    fix: grant user permissions through group

--- terragrunt/aws/base/smtp.tf
+++ terragrunt/aws/base/smtp.tf
@@ -11,20 +11,31 @@ resource "aws_iam_access_key" "security_tools" {
   user = aws_iam_user.security_tools.name
 }
 
+resource "aws_iam_group_membership" "security_tools_automation" {
+  name = "SecurityToolsNotificationIAMGroupMembership"
+
+  users = [
+    aws_iam_user.security_tools.name,
+  ]
+
+  group = aws_iam_group.security_tools_automation.name
+}
+
+resource "aws_iam_group" "security_tools_automation" {
+  name = "automation-security-tools-notification-group"
+  path = "/automation/securitytools/"
+}
+
+resource "aws_iam_group_policy" "security_tools_automation_policy" {
+  name   = "automation-security-tools-notification-group-policy"
+  group  = aws_iam_group.security_tools_automation.name
+  policy = data.aws_iam_policy_document.security_tools_automation.json
+}
+
 # Attaches a Managed IAM Policy to SES Email Identity resource
-data "aws_iam_policy_document" "policy_document" {
+data "aws_iam_policy_document" "security_tools_automation" {
   statement {
     actions   = ["ses:SendEmail", "ses:SendRawEmail"]
     resources = [aws_ses_email_identity.security_tools.arn]
   }
-}
-
-resource "aws_iam_policy" "policy" {
-  name   = "SecurityToolsNotificationPolicy"
-  policy = data.aws_iam_policy_document.policy_document.json
-}
-
-resource "aws_iam_user_policy_attachment" "user_policy" {
-  user       = aws_iam_user.security_tools.name
-  policy_arn = aws_iam_policy.policy.arn
 }
