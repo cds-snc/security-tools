cb0766cc557fefad567c05915018dafe3b36f680
Mohamed Nur <mohamed.nur@cds-snc.ca>
2022-05-19 15:49:54 -0400

    chore: add makefile

---
+++ images/cloud_asset_inventory/sentinel_neo4j_forwarder/src/connector.py
@@ -0,0 +1,3 @@
+# This file exists to satisfy the import requirements
+# of the other files. The actual live code is found here
+# https://github.com/cds-snc/aws-sentinel-connector-layer/blob/main/layer/connector.py
