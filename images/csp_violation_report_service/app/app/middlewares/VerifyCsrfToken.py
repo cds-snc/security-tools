a7df0a3e7ed16251d05a4937a133c7f60180f00f
Mohamed Nur <mohamed.nur@cds-snc.ca>
2022-08-02 12:01:18 -0400

    feat: tighten post rules for proxy

--- images/csp_violation_report_service/app/app/middlewares/VerifyCsrfToken.py
+++ images/csp_violation_report_service/app/app/middlewares/VerifyCsrfToken.py
@@ -3,4 +3,4 @@ from masonite.middleware import VerifyCsrfToken as Middleware
 
 class VerifyCsrfToken(Middleware):
 
-    exempt = ["/"]
+    exempt = ["/report"]
--- images/csp_violation_report_service/app/routes/web.py
+++ images/csp_violation_report_service/app/routes/web.py
@@ -2,7 +2,7 @@ from masonite.routes import Route
 
 ROUTES = [
     Route.get("/", "ReportController@show").name("show"),
-    Route.post("/", "ReportController@save").name("save"),
+    Route.post("/report", "ReportController@save").name("save"),
     Route.post("/filter/domain", "ReportController@filter_domain").name("filter_domain"),
     Route.get("/healthcheck", "HealthController@show").name("healthcheck"),
     Route.get("/lang/@lang", "LangController@switch").name("language_switcher"),
