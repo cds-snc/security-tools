2082e6b0e92c1d648445134315e9b6dc169fe635
Alvaro Scheid <ascheid@gmail.com>
2022-10-03 13:36:37 -0400

    Fix/connector layer version (#78)
    * fix: bumping the connection version to an existing
    
    * fix: adding the version to other calls

--- bin/cloud_asset_inventory/get_sentinel_connector_layer.sh
+++ bin/cloud_asset_inventory/get_sentinel_connector_layer.sh
@@ -2,6 +2,6 @@
 
 aws lambda get-layer-version-by-arn \
 --region ca-central-1 \
---arn arn:aws:lambda:ca-central-1:283582579564:layer:aws-sentinel-connector-layer:6 \
+--arn arn:aws:lambda:ca-central-1:283582579564:layer:aws-sentinel-connector-layer:8 \
 | jq -r '.Content.Location' \
 | xargs curl -o ../../images/cloud_asset_inventory/sentinel_neo4j_forwarder/connector.zip
