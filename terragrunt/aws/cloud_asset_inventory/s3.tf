45d99ddce58fe01114f82d5d201ee2a566ce7a0d
Guillaume Charest <guillaume.charest@cds-snc.ca>
2023-03-24 14:03:19 +0000

    feat: update bucket config
    Signed-off-by: Guillaume Charest <guillaume.charest@cds-snc.ca>

--- terragrunt/aws/cloud_asset_inventory/s3.tf
+++ terragrunt/aws/cloud_asset_inventory/s3.tf
@@ -1,21 +1,15 @@
+locals {
+  cloudquery_name = "${local.name_prefix}-cloudquery-results"
+}
+
 module "cloudquery_s3_bucket" {
-  source = "github.com/cds-snc/terraform-modules?ref=v5.1.4//s3"
-  name   = "security-tools-794722365809-cloudquery-results"
+  source      = "github.com/cds-snc/terraform-modules?ref=v5.1.4//s3"
+  bucket_name = local.cloudquery_name
 
   versioning = true
 
-  lifecycle_rule {
-    enabled = true
-    id      = "delete-old-versions"
-    prefix  = ""
-
-    noncurrent_version_expiration {
-      days = 30
-    }
-  }
-
-  billing_tag_key    = var.billing_tag_key
-  billing_tag_value  = var.billing_tag_value
-  critical_tag_key   = var.critical_tag_key
-  critical_tag_value = var.critical_tag_value
+  billing_tag_key   = var.billing_tag_key
+  billing_tag_value = var.billing_tag_value
+  # critical_tag_key   = var.critical_tag_key
+  # critical_tag_value = var.critical_tag_value
 }
