45649b87f80569ad96b033a971bd358d6f51633b
Guillaume Charest <guillaume.charest@cds-snc.ca>
2023-04-13 19:07:50 +0000

    feat: add kms_key_arn for ref

--- terragrunt/aws/cloud_asset_inventory/s3.tf
+++ terragrunt/aws/cloud_asset_inventory/s3.tf
@@ -1,18 +1,17 @@
-locals {
-  name_prefix     = "${var.product_name}-${var.account_id}"
-  cloudquery_name = "${local.name_prefix}-cloudquery-results"
-}
-
 module "cloudquery_s3_bucket" {
   source      = "github.com/cds-snc/terraform-modules?ref=v5.1.4//S3"
   bucket_name = local.cloudquery_name
 
   billing_tag_key   = var.billing_tag_key
   billing_tag_value = var.billing_tag_value
-  # critical_tag_key   = var.critical_tag_key
-  # critical_tag_value = var.critical_tag_value
+
+  kms_key_arn = data.aws_kms_key.s3_bucket_kms_key.arn
 
   versioning = {
     enabled = true
   }
+}
+
+data "aws_kms_key" "s3_bucket_kms_key" {
+  key_id = "alias/aws/s3"
 }
