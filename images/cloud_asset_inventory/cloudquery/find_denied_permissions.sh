829ffd435a0b3b8fb7530b520fb8b1e00c4f1106
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2023-04-14 15:17:11 -0400

    fix: change CQ destination file format to JSON (#262)
    * fix: change CQ destination file format to JSON
    
    * fix: change path suffix to .json

--- images/cloud_asset_inventory/cloudquery/find_denied_permissions.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Script to find all AWS permissions that were denied in a log file
-
-# Usage: ./find_denied_permissions.sh <log_file>
-# Output: denied_permissions.txt
-
-# Permissions denied appear after the string pattern "not authorized to perform:" in the log file
-grep "not authorized to perform:" "$1" | grep -E -io "([A-z0-9]+:[A-z]+) " | sort | uniq > denied_permissions.txt
