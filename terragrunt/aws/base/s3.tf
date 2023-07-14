79eb907eb1bdf2c49545bf095f2805aecbfb32fc
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2023-07-14 09:32:01 -0700

    Changing the terraform module reference links (#326)

--- terragrunt/aws/base/s3.tf
+++ terragrunt/aws/base/s3.tf
@@ -4,7 +4,7 @@ locals {
 }
 
 module "log_bucket" {
-  source            = "github.com/cds-snc/terraform-modules?ref=v0.0.47//S3_log_bucket"
+  source            = "github.com/cds-snc/terraform-modules//S3_log_bucket?ref=v7.0.0"
   bucket_name       = "${local.name_prefix}-logs"
   billing_tag_value = var.billing_tag_value
 }
