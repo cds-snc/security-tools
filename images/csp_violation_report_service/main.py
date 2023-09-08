0185184ff7aa92398ae49d182599de2927f2dcca
Pat Heard <patrick.heard@cds-snc.ca>
2023-09-08 15:44:12 -0400

    fix: handle base64 encoded event body (#356)
    Update the function to handle a base64 encoded event body attribute.
    
    Update the sentinel forwarder's log pattern.
    
    Simplify the devcontainer definition.
    
    Add a CI workflow to lint, format check and run unit tests.

--- images/csp_violation_report_service/main.py
+++ images/csp_violation_report_service/main.py
@@ -1,9 +1,27 @@
 """
 Log csp-reports
 """
+import base64
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 
 def handler(event, context):
-    print(f"Received request: {event}")
-    if "body" in event and "csp-report" in event["body"]:
-        print(event["body"])
+    "Handle the CSP report POST requests"
+
+    logger.debug("Received request: %s", event)
+    event_body = event.get("body")
+
+    if event_body:
+        # Handle base64 encoded POST body content
+        if event.get("isBase64Encoded"):
+            event_body = base64.b64decode(event_body).decode("utf-8")
+
+        # Log the csp-report
+        if "csp-report" in event_body:
+            # print is used so as to only print the raw JSON to CloudWatch logs
+            print(event_body)
+
     return {"status": "ok"}
