50487a9517626a60a0fe2056d3ace3c4fb0baaed
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2024-04-04 18:10:22 +0000

    fix: append logs to existing logs

--- images/cloud_asset_inventory/cloudquery/dev/log_connections.sh
+++ images/cloud_asset_inventory/cloudquery/dev/log_connections.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tcpdump -nn -i any dst port not 22 and dst net not 127.0.0.0/8 > /var/log/connection_logs.txt 2>&1 
+tcpdump -nn -i any dst port not 22 and dst net not 127.0.0.0/8 >> /var/log/connection_logs.txt 2>&1 
