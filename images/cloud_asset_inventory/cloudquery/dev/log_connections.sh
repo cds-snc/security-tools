b782c89e0ec6c47cde18461f05017df8a4b44c9f
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2024-04-04 18:01:18 +0000

    Update to redirect all tcpdump output w/o filtering

--- images/cloud_asset_inventory/cloudquery/dev/log_connections.sh
+++ images/cloud_asset_inventory/cloudquery/dev/log_connections.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tcpdump -nn -i any dst port not 22 and dst net not 127.0.0.0/8 | awk '{print $3}' | awk -F'.' '{print $1"."$2"."$3"."$4}' | sort -u
+tcpdump -nn -i any dst port not 22 and dst net not 127.0.0.0/8 > /var/log/connection_logs.txt 2>&1 
