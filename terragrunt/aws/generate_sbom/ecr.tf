3b76c69b3da2717d72ba0655512410315f1c0560
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2024-10-02 16:43:07 +0000

    Deleting old private ECR code

--- terragrunt/aws/generate_sbom/ecr.tf
+++ terragrunt/aws/generate_sbom/ecr.tf
@@ -1,22 +1,3 @@
-resource "aws_ecr_repository" "generate_sbom" {
-  name                 = "${var.product_name}/generate_sbom/trivy"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-
-  encryption_configuration {
-    encryption_type = "KMS"
-  }
-
-  tags = {
-    (var.billing_tag_key) = var.billing_tag_value
-    Terraform             = true
-    Product               = "${var.product_name}-${var.tool_name}"
-  }
-}
-
 resource "aws_ecrpublic_repository" "generate_sbom_public" {
   provider        = aws.us-east-1
   repository_name = "${var.product_name}/generate_sbom/trivy"
