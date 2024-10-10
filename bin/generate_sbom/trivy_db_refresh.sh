380d0a0463079cc5c35f451e17b0a6b1dfa26ad1
Pat Heard <patrick.heard@cds-snc.ca>
2024-10-10 10:58:23 -0400

    feat: add nightly job to refresh Trivy database
    Add a nightly workflow that fetches the latest Trivy database.  This is then
    used by the CDS repos performing Docker scans.

---
+++ bin/generate_sbom/trivy_db_refresh.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#
+# Attempts to refresh the Trivy Database in the specified target repository.
+# Uses backoff strategy to retry on failure since there have been intermittent rate
+# limiting issues with the source ECR public registry.
+#
+# This script expects that ECR authentication has already been performed.
+#
+
+target_repo="$1"
+max_attempts=5
+attempt=0
+backoff=1
+
+while [ $attempt -lt $max_attempts ]; do
+if oras cp public.ecr.aws/aquasecurity/trivy-db:latest "$target_repo"; then
+    echo "Trivy Database refreshed successfully."
+    break
+else
+    attempt=$((attempt + 1))
+    echo "Attempt $attempt failed. Retrying in $backoff seconds..."
+    sleep $backoff
+    backoff=$((backoff * 2))
+fi
+done
+
+if [ $attempt -eq $max_attempts ]; then
+    echo "Failed to refresh Trivy Database after $max_attempts attempts."
+    exit 1
+fi
