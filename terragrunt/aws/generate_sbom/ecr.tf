017ee4fe5ce9f2125075afc02fa3118d50cf4b6a
Pat Heard <patrick.heard@cds-snc.ca>
2024-10-18 08:48:49 -0400

    fix: add Trivy Java DB ECR and refresh job
    Add a new ECR to hold the Trivy Java DB as this has also started
    timing out with TOOMANYREQUEST errors.

--- terragrunt/aws/generate_sbom/ecr.tf
+++ terragrunt/aws/generate_sbom/ecr.tf
@@ -1,19 +1,30 @@
-resource "aws_ecrpublic_repository" "generate_sbom_public" {
+resource "aws_ecrpublic_repository" "generate_sbom_trivy_db" {
   provider        = aws.us-east-1
   repository_name = "${var.product_name}/generate_sbom/trivy-db"
+
   tags = {
     (var.billing_tag_key) = var.billing_tag_value
     Terraform             = true
     Product               = "${var.product_name}-${var.tool_name}"
   }
 }
+
+resource "aws_ecrpublic_repository" "generate_sbom_trivy_java_db" {
+  provider        = aws.us-east-1
+  repository_name = "${var.product_name}/generate_sbom/trivy-java-db"
+
+  tags = {
+    (var.billing_tag_key) = var.billing_tag_value
+    Terraform             = true
+    Product               = "${var.product_name}-${var.tool_name}"
+  }
+}
+
 data "aws_iam_policy_document" "sbom_public_policy_document" {
   provider = aws.us-east-1
   statement {
     sid    = "sbom_public_policy"
     effect = "Allow"
-
-
     principals {
       type        = "AWS"
       identifiers = ["*"]
@@ -40,8 +51,43 @@ data "aws_iam_policy_document" "sbom_public_policy_document" {
     }
   }
 }
-resource "aws_ecrpublic_repository_policy" "sbom_public_policy" {
+
+#
+# Attach the ECR IAM policy
+#
+resource "aws_ecrpublic_repository_policy" "generate_sbom_trivy_db" {
   provider        = aws.us-east-1
-  repository_name = aws_ecrpublic_repository.generate_sbom_public.repository_name
+  repository_name = aws_ecrpublic_repository.generate_sbom_trivy_db.repository_name
   policy          = sensitive(data.aws_iam_policy_document.sbom_public_policy_document.json)
+}
+
+resource "aws_ecrpublic_repository_policy" "generate_sbom_trivy_java_db" {
+  provider        = aws.us-east-1
+  repository_name = aws_ecrpublic_repository.generate_sbom_trivy_java_db.repository_name
+  policy          = sensitive(data.aws_iam_policy_document.sbom_public_policy_document.json)
+}
+
+#
+# Policy to expire untagged images
+#
+resource "aws_ecr_lifecycle_policy" "generate_sbom_trivy_db" {
+  provider   = aws.us-east-1
+  repository = aws_ecr_repository.generate_sbom_trivy_db.name
+  policy     = file("${path.module}/policy/lifecycle.json")
+}
+
+resource "aws_ecr_lifecycle_policy" "generate_sbom_trivy_java_db" {
+  provider   = aws.us-east-1
+  repository = aws_ecr_repository.generate_sbom_trivy_java_db.name
+  policy     = file("${path.module}/policy/lifecycle.json")
+}
+
+moved {
+  from = aws_ecr_repository.generate_sbom_public
+  to   = aws_ecr_repository.generate_sbom_trivy_db
+}
+
+moved {
+  from = aws_ecrpublic_repository_polic.sbom_public_policy
+  to   = aws_ecrpublic_repository_polic.generate_sbom_trivy_db
 }
