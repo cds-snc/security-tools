e44a6b0317640ef77466c8c0cadc351e8ea37e6a
Guillaume <guillaume.charest@cds-snc.ca>
2026-05-29 19:48:50 +0000

    fix: replace step function by simple scheduled task

--- terragrunt/aws/cloud_asset_inventory/cartography.tf
+++ terragrunt/aws/cloud_asset_inventory/cartography.tf
@@ -45,3 +45,87 @@ resource "aws_cloudwatch_log_group" "cartography" {
     Product               = "${var.product_name}-${var.tool_name}"
   }
 }
+
+
+
+# CloudWatch Event Rule to trigger Cartography ECS task directly
+resource "aws_cloudwatch_event_rule" "asset_inventory_cartography" {
+  name                = "cartography"
+  schedule_expression = "cron(0 22 * * ? *)"
+  state               = "ENABLED"
+
+  tags = {
+    (var.billing_tag_key) = var.billing_tag_value
+    Terraform             = true
+    Product               = "${var.product_name}-${var.tool_name}"
+  }
+}
+
+resource "aws_cloudwatch_event_target" "cartography_ecs" {
+  rule     = aws_cloudwatch_event_rule.asset_inventory_cartography.name
+  arn      = aws_ecs_cluster.cloud_asset_discovery.arn
+  role_arn = aws_iam_role.cartography_events.arn
+  ecs_target {
+    task_count          = 1
+    launch_type         = "FARGATE"
+    task_definition_arn = aws_ecs_task_definition.cartography.arn
+    network_configuration {
+      subnets          = var.vpc_private_subnet_ids
+      security_groups  = [aws_security_group.cartography.id]
+      assign_public_ip = false
+    }
+    # Optionally override command/args here if needed
+    # overrides = jsonencode({
+    #   containerOverrides = [{
+    #     name = local.cartography_service_name
+    #     command = [
+    #       "cartography",
+    #       "--neo4j-uri", "bolt://neo4j.internal.local:7687",
+    #       "--neo4j-user", "neo4j",
+    #       "--neo4j-password-env-var", "NEO4J_SECRETS_PASSWORD",
+    #       "--aws-sync-all-profiles",
+    #       "--aws-best-effort-mode"
+    #     ]
+    #   }]
+    # })
+  }
+}
+
+resource "aws_iam_role" "cartography_events" {
+  name = "cartographyEventsRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "events.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+  tags = {
+    (var.billing_tag_key) = var.billing_tag_value
+    Terraform             = true
+    Product               = "${var.product_name}-${var.tool_name}"
+  }
+}
+
+resource "aws_iam_role_policy" "cartography_events" {
+  name = "cartographyEventsPolicy"
+  role = aws_iam_role.cartography_events.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecs:RunTask",
+          "iam:PassRole"
+        ]
+        Resource = [
+          aws_ecs_task_definition.cartography.arn,
+          aws_iam_role.cartography_task_execution_role.arn,
+          aws_iam_role.cartography_container_execution_role.arn
+        ]
+      }
+    ]
+  })
+}
